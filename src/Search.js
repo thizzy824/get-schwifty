5ea641f87355644359fe20cf2c586f6ab27e094c
thizzy824 <agpastora7@gmail.com>
2022-09-30 13:55:41 -0700

    created handleChange and handleSubmit functions for Search component/form

--- src/Search.js
+++ src/Search.js
@@ -1,15 +1,29 @@
 import React from 'react';
+import {useState} from 'react'
 
 function Search() {
+
+    const [searchString, setSearchString] = useState('')
+
+    function handleChange(event) {
+        setSearchString(event.target.value)
+    }
+
+    function handleSubmit(event) {
+        event.preventDefault()
+        console.log(searchString)
+    }
+
 	return (
 		<div>
 			<h2 className='search-title'>Search for any character in the Rick and Morty Universe!</h2>
-			<form>
+			<form onSubmit={handleSubmit}>
                 <label htmlFor='searchString'> Search : </label>
                 <input 
                 type='text'
                 id='searchString'
                 name='searchString'
+                onChange={handleChange}
                 required
                 />
                 <button type='submit'>Submit</button>

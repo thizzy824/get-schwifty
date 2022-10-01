4ce38f15a01d0a91331bd60427e8bd5e81999ee4
thizzy824 <agpastora7@gmail.com>
2022-10-01 11:40:15 -0700

    created Results component

---
+++ src/Results.js
@@ -0,0 +1,16 @@
+import {useEffect, useState} from 'react'
+
+function Results(props) {
+
+    const [results, setResults] = useState(null)
+    const [error, setError] = useState(false)
+
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default Results;

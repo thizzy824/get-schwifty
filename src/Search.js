2ad57de2aa7cefc29989051c7cc53f047ecfe1bd
thizzy824 <agpastora7@gmail.com>
2022-10-01 11:43:45 -0700

    refactored code and added path to routes

--- src/Search.js
+++ src/Search.js
@@ -17,8 +17,8 @@ function Search() {
 	return (
 		<div>
 			<h2 className='search-title'>Search for any character in the Rick and Morty Universe!</h2>
-			<form onSubmit={handleSubmit}>
-                <label htmlFor='searchString'> Search : </label>
+			<form className='search-form' onSubmit={handleSubmit}>
+                <label htmlFor='searchString'> <strong>Search:</strong></label>
                 <input 
                 type='text'
                 id='searchString'

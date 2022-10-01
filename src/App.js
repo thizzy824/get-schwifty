b2bd2acad9c332731cbbd72674f40299c776867b
thizzy824 <agpastora7@gmail.com>
2022-10-01 11:42:23 -0700

    imported Routes, Route, and useSearchParams

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
 import './App.css';
 import Header from './Header';
 import Search from './Search';
+import Results from './Results';
+import {Routes, Route, Link, useSearchParams} from 'react-router-dom'
+import { useState } from 'react';
 
 function App() {
+
+  const [searchParams, setSearchParams] = useSearchParams()
+
 	return (
-  <>
-  <div className='schwifty-container'>
-    <Header />
-    <Search/>
-  </div>
-  </>
-  )
+		<>
+			<div className='schwifty-container'>
+				<Link to='/'>
+					<Header />
+				</Link>
+			</div>
+			<Routes>
+				<Route
+					path='/'
+					element={<Search setSearchParams={setSearchParams} />}
+				/>
+				<Route
+					path='/results'
+					element={<Results searchParams={searchParams} />}
+				/>
+			</Routes>
+		</>
+	);
 }
 
 export default App;

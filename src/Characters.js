c42fdfa639f3922a4936f94779f2b28c6fddea97
thizzy824 <agpastora7@gmail.com>
2022-10-04 13:35:09 -0700

    created Characters component to hold both Search/Results components

---
+++ src/Characters.js
@@ -0,0 +1,39 @@
+import Search from './Search';
+import Results from './Results';
+import { useState, useEffect } from 'react';
+import {Link} from 'react-router-dom'
+
+
+import React from 'react';
+
+function Characters() {
+	const [characters, setCharacters] = useState([]);
+	const [searchString, setSearchString] = useState('');
+
+	let url = `https://rickandmortyapi.com/api/character/?name=${searchString}`;
+
+	useEffect(() => {
+		fetch(url)
+			.then((res) => res.json())
+			.then((data) => setCharacters(data.results))
+			.catch((err) => console.log('Error!', err));
+	}, [url]);
+
+	function handleChange(event) {
+		setSearchString(event.target.value);
+	}
+
+	return (
+		<div>
+			<div>
+				<Link to='/episodes'>
+					<h2 className='center'>Episodes</h2>
+				</Link>
+			</div>
+			<Search handleChange={handleChange} searchString={searchString} />
+			<Results characters={characters} />
+		</div>
+	);
+}
+
+export default Characters;

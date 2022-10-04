37f12e67a522ebc07de974eaa02db3575c41479a
thizzy824 <agpastora7@gmail.com>
2022-10-04 15:09:22 -0700

    created EpisodeResults component

---
+++ src/EpisodeResults.js
@@ -0,0 +1,25 @@
+
+const EpisodeResults = ({episodes}) => {
+
+    if (!episodes) {
+        return <h2 className='not-found'>No Episode Found</h2>
+    }
+
+    return (
+        <div>
+            <div className='results'>
+                {episodes.map((episode) => {
+                    <div key={episode.id} className='details' >
+                        <strong>
+                            <p>Episode # : {episode.id}</p>
+                            <p>Name: {episode.name}</p>
+                            <p>Air-Date: {episode.air_date}</p>
+                        </strong>
+                    </div>
+                })}
+            </div>
+        </div>
+    );
+}
+
+export default EpisodeResults;
